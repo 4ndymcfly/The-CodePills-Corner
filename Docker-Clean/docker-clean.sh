2a17ed3b53cafc5e2d6794ffd645236ff04c3c22
Andy McFly <30553433+4ndymcfly@users.noreply.github.com>
2023-05-30 10:59:53 +0200

    Update docker-clean.sh

--- Docker-Clean/docker-clean.sh
+++ Docker-Clean/docker-clean.sh
@@ -1,1 +1,8 @@
-
+function cleanDocker(){
+  docker stop $(docker ps -a -q)
+  docker rm $(docker ps -a -q) 2>/dev/null
+  docker rmi $(docker images -q) 2>/dev/null
+  docker volume rm $(docker volume ls -q) 2>/dev/null
+  docker network rm $(docker network ls -q) 2>/dev/null
+  docker system prune --all --volumes -f
+}
