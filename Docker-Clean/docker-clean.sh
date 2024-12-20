1ee6bbb047e1d1c489b7cc5dee90899a6cca8a87
Andrés Lorente <4ndymcfly@gmail.com>
2024-12-20 14:09:22 +0100

    Update docker-clean.sh

--- Docker-Clean/docker-clean.sh
+++ Docker-Clean/docker-clean.sh
@@ -1,8 +1,14 @@
+# Borra uno por uno los objetos de Docker que no están en uso (quitar lo que no queremos borrar)
 function cleanDocker(){
-  docker stop $(docker ps -a -q)
   docker rm $(docker ps -a -q) 2>/dev/null
   docker rmi $(docker images -q) 2>/dev/null
   docker volume rm $(docker volume ls -q) 2>/dev/null
   docker network rm $(docker network ls -q) 2>/dev/null
+}
+
+# Borra todo completamente
+function cleanDockerAll(){
+  docker stop $(docker ps -q) 2>/dev/null
   docker system prune --all --volumes -f
 }
+
